4ed10c019e614a710fc04a1a324c8fadcd8105fd
IshamIqbal <isham.iqbal@guildhawk.com>
2023-01-09 14:27:32 +0000

    Implemented test for POST: bar/ endpoint

--- polls/tests.py
+++ polls/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+from polls.models import Bar
+
+class BarTestCase(TestCase):
+
+    def setUp(self):
+
+        self.data = {"value": "test"}
+    
+    def test_bar_insertion(self):
+
+        self.client.post("/api/v1/bar/", data=self.data, format="json")
+        
+        self.client.post("/api/v1/bar/", data=self.data, format="json")
+        
+
